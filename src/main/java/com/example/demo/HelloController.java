9105169bfa1a751702d86ce4e9acb9f40e8350b5
priyakavade11 <priyakavade11@gmail.com>
2025-11-19 01:20:04 +0000

    Add files via upload

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(defaultValue = "world") String name) {
+        log.debug("Handling /hello request for name={} ", name);
+        log.info("Saying hello to {}", name);
+        return "Hello, " + name + "!";
+    }
+}
